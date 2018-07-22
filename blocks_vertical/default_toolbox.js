8942bdb53e9d790b423080fb32d3ef6f21412fb2
SungBeom <psb_94@naver.com>
2018-07-22 13:18:30 +0900

    loop 수정

--- blocks_vertical/default_toolbox.js
+++ blocks_vertical/default_toolbox.js
@@ -296,13 +296,13 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
     '<block type="control_wait_until" id="control_wait_until"></block>' +
     '<block type="control_repeat_until" id="control_repeat_until"></block>' +
     '<block type="control_stop" id="control_stop"></block>' +
-    '<block type="control_start_as_clone" id="control_start_as_clone"></block>' +
-    '<block type="control_create_clone_of" id="control_create_clone_of">' +
-      '<value name="CLONE_OPTION">' +
-        '<shadow type="control_create_clone_of_menu"></shadow>' +
-      '</value>' +
-    '</block>' +
-    '<block type="control_delete_this_clone" id="control_delete_this_clone"></block>' +
+    // '<block type="control_start_as_clone" id="control_start_as_clone"></block>' +
+    // '<block type="control_create_clone_of" id="control_create_clone_of">' +
+    //   '<value name="CLONE_OPTION">' +
+    //     '<shadow type="control_create_clone_of_menu"></shadow>' +
+    //   '</value>' +
+    // '</block>' +
+    // '<block type="control_delete_this_clone" id="control_delete_this_clone"></block>' +
   '</category>' +
   '<category name="Texts" id="texts_extensions" colour="#ABABAB" secondaryColour="#8B8B8B">' +
     '<block type="texts_join" id="texts_join">' +
